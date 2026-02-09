02dcf99021adfc55cd06f6de546d098b8adc6551
Kevin2.0 <163003196+Kode-god@users.noreply.github.com>
2026-02-09 20:34:58 +0300

    Simplify CORS configuration in server.js
    Removed unnecessary methods and allowedHeaders from CORS configuration.

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,6 @@ const app = express();
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.options("*", cors());
